59f4afd9967ea4823aeb3afda95ae5f0d6b22576
Mathias Baert <mathiasbaert@gmail.com>
2010-11-28 01:08:37 +0100

    added licence: GPL3

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
+// iRail Website, mobile and desktop; Get public transit information for Belgium
+// Copyright (C) 2010 Mathias Baert
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // iRail Web - Mathias Baert 2010 - Some kind of free licence
-//
-// IRail.data: Non-dynamic data used in the iRail client
-// Railway station data sourced from original iRail client
+
+// App: manages the GUI and user interaction
 
 (function(){
 
